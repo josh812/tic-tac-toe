898d7f391b2dc1b228b275379282463c81591eb7
Josh <joshua.mccrory@icloud.com>
2021-10-18 18:34:55 -0700

    Added display

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+const container = document.querySelector('#container');
+const board = document.querySelector('#board');
+
 const gameBoard = (() => {
-    const boardArray = [' ', ' ', ' ', 
-                        ' ', ' ', ' ',
-                        ' ', ' ', ' '];
-    return {};
+    const boardArray = ['X', 'O', 'O', 
+                        'O', 'X', 'X',
+                        'X', 'X', 'O'];
+    return {boardArray};
 })();
 
 const displayController = (() => {
-    
+    function displayGameBoard(gameBoard) {
+        for(let i=0; i<gameBoard.boardArray.length; i++) {
+            const boardPara = document.createElement('p');
+            boardPara.setAttribute('class', 'boardSquare');
+            boardPara.textContent = gameBoard.boardArray[i];
+            board.appendChild(boardPara);
+        }
+    }
+    return {displayGameBoard};
 })();
 
 const playerFactory = (name) => {
     return {name};
 }
+
+displayController.displayGameBoard(gameBoard);
