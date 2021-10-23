dada26b796da8ab898a8bc73b57e55f0a9d9a153
Josh <joshua.mccrory@icloud.com>
2021-10-22 19:54:18 -0700

    Added basic clicking functions

--- script.js
+++ script.js
@@ -2,26 +2,72 @@ const container = document.querySelector('#container');
 const board = document.querySelector('#board');
 
 const gameBoard = (() => {
-    const boardArray = ['X', 'O', 'O', 
-                        'O', 'X', 'X',
-                        'X', 'X', 'O'];
-    return {boardArray};
+    const boardArray = [' ', ' ', ' ', 
+                        ' ', ' ', ' ',
+                        ' ', ' ', ' '];
+    function markBoardSquare(turn, spaceNum) {
+        boardArray[spaceNum] = turn;
+    }
+
+    return {boardArray, markBoardSquare};
 })();
 
+
 const displayController = (() => {
-    function displayGameBoard(gameBoard) {
-        for(let i=0; i<gameBoard.boardArray.length; i++) {
+    let turn = "X";
+
+    function displayGameBoard() {
+        board.textContent = "";
+        for(let i=0; i < gameBoard.boardArray.length; i++) {
+            const boardDiv = document.createElement('div');
+            boardDiv.setAttribute('class', 'boardSquare');
+            boardDiv.setAttribute('id', i);
             const boardPara = document.createElement('p');
-            boardPara.setAttribute('class', 'boardSquare');
             boardPara.textContent = gameBoard.boardArray[i];
-            board.appendChild(boardPara);
+            board.appendChild(boardDiv);
+            boardDiv.appendChild(boardPara);
+        }
+        
+        if(document.querySelector('#turn-para') != undefined) {
+            document.querySelector('#turn-para').textContent = `${turn}'s turn!`;
+        } else {
+            const turnPara = document.createElement('p');
+            turnPara.setAttribute('id', 'turn-para');
+            turnPara.textContent = `${turn}'s turn!`;
+            container.appendChild(turnPara);
         }
     }
-    return {displayGameBoard};
+
+    function changeTurn(currentTurn) {
+        if(turn === "X") {
+            turn = "O";
+        } else if(turn === "O") {
+            turn = "X";
+        }
+    }
+
+    function fullTurn(e) {
+        gameBoard.markBoardSquare(turn, e.target.getAttribute('id'));
+        changeTurn(turn);
+        displayGameBoard();
+        const squares = document.querySelectorAll('.boardSquare');
+        squares.forEach(square => {
+            square.removeEventListener('click', displayController.fullTurn);
+            square.addEventListener('click', displayController.fullTurn);
+        });
+    }
+
+    return {displayGameBoard, fullTurn};
 })();
+
 
 const playerFactory = (name) => {
     return {name};
 }
 
-displayController.displayGameBoard(gameBoard);
+displayController.displayGameBoard();
+
+const squares = document.querySelectorAll('.boardSquare');
+squares.forEach(square => {
+    square.addEventListener('click', displayController.fullTurn);
+});
